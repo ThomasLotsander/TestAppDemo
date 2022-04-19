67e21f0922a162724c190b7e3fc6a43f0da02d16
Thomas Lotsander <thomas.lotsander@soprasteria.com>
2022-04-19 20:48:54 +0200

    Better message

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Test");
-Console.WriteLine("Test 2");
-Console.WriteLine("test 2.1");
+string test = "Thomas";
 Console.ReadLine();
+
+
+// Comment
