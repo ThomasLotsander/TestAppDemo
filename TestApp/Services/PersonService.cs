935b6a4ab676c8439606708b4161748aa448fac0
Thomas Lotsander <thomas.lotsander@soprasteria.com>
2022-04-19 13:41:44 +0200

    Add person model

---
+++ TestApp/Services/PersonService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Models;
+
+namespace TestApp.Services
+{
+    internal class PersonService
+    {
+        public void SavePseronName(Person person)
+        {
+            // save name on person 
+        }
+    }
+}
