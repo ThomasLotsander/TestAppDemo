50b87f8e9144bbfbae39e400030470f7357a7d7d
Thomas Lotsander <thomas.lotsander@soprasteria.com>
2022-04-19 13:37:06 +0200

    Better message for everyone

---
+++ TestApp/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    internal class Class1
+    {
+        public int GetNumber()
+        {
+            return 10;
+        }
+
+        public string GetString()
+        {
+            return "Hello World!";
+        }
+    }
+}
