7e268f83b84b67e9c6e5605eb7c1aef32af8e2ea
Thomas Lotsander <thomas.lotsander@soprasteria.com>
2022-04-19 13:33:23 +0200

    commit again

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Test");
+Console.WriteLine("Test 2");
+Console.WriteLine("test 2.1");
