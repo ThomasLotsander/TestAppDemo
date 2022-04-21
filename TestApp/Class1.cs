98329648f5f385d183bdd8e4bee2c337a556e30e
Thomas Lotsander <thomas.lotsander@rikstv.no>
2022-04-21 13:23:14 +0200

    update

--- TestApp/Class1.cs
+++ TestApp/Class1.cs
@@ -19,6 +19,8 @@ namespace TestApp
             var tes = "TEST--Test";            
             var test = tes;
             test = 12345;
+
+            // test
             return "Hello World!";
         }
     }
